d64d6c5526f6d32dbd4688b44b11fab05d462152
DjMhel <aclaogloryzann30@gmail.com>
2025-12-20 14:36:20 +0800

    ui core added

---
+++ ui_core.cpp
@@ -0,0 +1,5 @@
+//
+// Created by DjMhel on 20/12/2025.
+//
+
+#include "ui_core.h"
---
+++ ui_core.h
@@ -0,0 +1,13 @@
+//
+// Created by DjMhel on 20/12/2025.
+//
+
+#ifndef FINAL_PROJECT_2_UI_CORE_H
+#define FINAL_PROJECT_2_UI_CORE_H
+
+
+class ui_core {
+};
+
+
+#endif //FINAL_PROJECT_2_UI_CORE_H
